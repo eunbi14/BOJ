ad873370fb8ddf14344078b5126be3cd933a6e73
eunbi14 <53552966+eunbi14@users.noreply.github.com>
2020-11-08 10:08:17 +0900

    Add files via upload

---
+++ NUMBER (10000~)/게으른백곰_10025.java
@@ -0,0 +1,43 @@
+package 슬라이딩윈도우;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 게으른백곰_10025 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(bf.readLine());
+		int N = Integer.valueOf(st.nextToken());
+		int K = Integer.valueOf(st.nextToken());
+		int[] fence = new int[1000001];
+		int max = 0;
+		for(int i=0;i<N;i++) {
+			st = new StringTokenizer(bf.readLine());
+			int g = Integer.valueOf(st.nextToken());
+			int x = Integer.valueOf(st.nextToken());
+			fence[x] = g;
+			if(x>max) max = x;
+		}
+		
+		int answer = 0;
+		
+		for(int i=0;i<=2*K&&i<=1000000;i++) {
+			answer += fence[i];
+		}
+		
+		int tmp = answer;
+		for(int i=0;i<=max-(2*K)&&i<=1000000;) {
+			tmp -= fence[i];
+			i++;
+			tmp += fence[i+(2*K)];
+			if(tmp>answer) answer = tmp;
+		}
+		
+		System.out.println(answer);
+	}
+
+}
